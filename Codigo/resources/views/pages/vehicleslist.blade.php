e1daffb92b0d402a14915c9cac04fd34c3c31d8b
lcs2001 <lcs2001_@hotmail.com>
2021-03-27 10:15:48 -0300

    Updated CSS

--- Codigo/resources/views/pages/vehicleslist.blade.php
+++ Codigo/resources/views/pages/vehicleslist.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.standard')
 
 @section('extraassets')
-    <script src="{{ url('/assets/css/vehiclelist.css') }}"></script>
+    <link rel="stylesheet" href="{{ url('/assets/css/vehiclelist.css') }}" type="text/css">
     <script src="{{ url('/assets/js/sorttable.js') }}"></script> <!-- To sort table by headers -->
 @endsection
 
@@ -10,7 +10,7 @@
     <div id="main" class="row">
         <div class="col-12">
 
-            <div class="row mt-4 mb-2" style="padding-left: 5%; padding-right: 5%;">
+            <div id="filtros">
                 <div class="col-2">
                     <select class="form-select" aria-label="Default select example">
                         <option selected>Open this select menu</option>
@@ -21,11 +21,11 @@
                 </div>
             </div>
 
-            <div class="row" style="padding-left: 5%; padding-right: 5%;">
+            <div id="tabela" class="row">
                 <div class="col-12">
                     <div class="d-flex justify-content-center align-items-center w-100">
                         <table class="table table-dark sortable" style="border: 1px solid white; vertical-align: middle; text-align:center; position: relative;">
-                            <thead style="background-color: #252531;">
+                            <thead>
                                 <tr>
                                     <th scope="col">Placa</th>
                                     <th scope="col">Modelo</th>
@@ -39,15 +39,15 @@
                             </thead>
                             <tbody>
                                 <tr>
-                                    <th style="padding: 20px;" scope="row">AAA-1111</th>
-                                    <td style="padding: 20px;">Ford Ka</td>
-                                    <td style="padding: 20px;">Vermelho</td>
-                                    <td style="padding: 20px;">1</td>
-                                    <td style="padding: 20px;">Porteiro x</td>
-                                    <td style="padding: 20px;">  05:45</td>
-                                    <td style="padding: 20px;">06:30</td>
-                                    <td style="padding: 20px;">
-                                        <button style="background-color: #5C5C68; border-radius: 8px; padding: 5px;"><i class="fas fa-edit" style="color: #B4B4B4; padding: 10px;"></i></button>
+                                    <td scope="row">AAA-1111</th>
+                                    <td>Ford Ka</td>
+                                    <td>Vermelho</td>
+                                    <td>1</td>
+                                    <td>Porteiro x</td>
+                                    <td>05:45</td>
+                                    <td>06:30</td>
+                                    <td>
+                                        <button><i id="action" class="fas fa-edit"></i></button>
                                     </td>
                                 </tr>
                             </tbody>
