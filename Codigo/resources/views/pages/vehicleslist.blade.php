adb27192ee1d76d7311f14376ddcbb5f02f9bfd8
lcs2001 <lcs2001_@hotmail.com>
2021-03-28 22:16:52 -0300

    Vehicle list finished

--- Codigo/resources/views/pages/vehicleslist.blade.php
+++ Codigo/resources/views/pages/vehicleslist.blade.php
@@ -11,12 +11,12 @@
         <div class="col-12">
 
             <div id="filtros">
-                <div id="filtro-placa" class="col-2">
+                <div id="placa" class="col-2 desktop">
                     <label>Placa</label>
                     <label for="inputPlaca"></label>
                     <input type="text" class="form-control" id="inputPlaca" placeholder="Placa">
                 </div>    
-                <div id="filtro-portaria" class="col-2">
+                <div id="portaria" class="col-2 desktop">
                     <label>Portaria</label>
                     <select class="form-select">
                         <option selected>Selecione</option>
@@ -24,7 +24,7 @@
                         <option value="2">2</option>
                     </select>
                 </div>
-                <div id="filtro-porteiro" class="col-2">
+                <div id="porteiro" class="col-2 desktop">
                     <label>Porteiro</label>
                     <select class="form-select">
                         <option selected>Selecione</option>
@@ -32,11 +32,14 @@
                         <option value="2">José</option>
                     </select>
                 </div>
-                <div id="filtro-avancado" class="col-2">
-                    <button class="btn btn-secondary mb-2"><i id="action" class="fas fa-filter"></i></button>
+                <div id="filtro-avancado" class="desktop" class="col-1">
+                    <button class="btn btn-secondary"><i id="botoes" class="fas fa-filter"></i></button>
                 </div>
-                <div id="filtro-buscar" class="col-1">
-                    <button class="btn btn-secondary mb-2"><i id="action" class="fas fa-search"></i>Buscar</button>
+                <div id="filtro-avancado" class="mobile" class="col-6">
+                    <button class="btn btn-secondary"><i id="botoes" class="fas fa-filter"></i>Filtro Avançado</button>
+                </div>
+                <div id="buscar" class="col-1 desktop">
+                    <button class="btn btn-secondary"><i id="botoes" class="fas fa-search"></i><b class="responsive">Buscar</b></button>
                 </div>
             </div>
 
@@ -47,26 +50,26 @@
                             <thead>
                                 <tr>
                                     <th scope="col">Placa</th>
-                                    <th scope="col">Modelo</th>
-                                    <th scope="col">Cor</th>
-                                    <th scope="col">Portaria</th>
-                                    <th scope="col">Porteiro</th>
-                                    <th scope="col">Hora de Entrada</th>
-                                    <th scope="col">Hora de Saída</th>
+                                    <th class="desktop" scope="col">Modelo</th>
+                                    <th class="desktop" scope="col">Cor</th>
+                                    <th class="desktop" scope="col">Portaria</th>
+                                    <th class="desktop" scope="col">Porteiro</th>
+                                    <th class="desktop" scope="col">Hora de Entrada</th>
+                                    <th class="desktop" scope="col">Hora de Saída</th>
                                     <th scope="col">Ações</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
                                     <td scope="row">AAA-1111</th>
-                                    <td>Ford Ka</td>
-                                    <td>Vermelho</td>
-                                    <td>1</td>
-                                    <td>Porteiro x</td>
-                                    <td>05:45</td>
-                                    <td>06:30</td>
+                                    <td class="desktop">Ford Ka</td>
+                                    <td class="desktop">Vermelho</td>
+                                    <td class="desktop">1</td>
+                                    <td class="desktop">Porteiro x</td>
+                                    <td class="desktop">05:45</td>
+                                    <td class="desktop">06:30</td>
                                     <td>
-                                        <button lass="btn btn-secondary"><i id="action" class="fas fa-edit"></i></button>
+                                        <button lass="btn btn-secondary"><i id="botoes" class="fas fa-edit"></i></button>
                                     </td>
                                 </tr>
                             </tbody>
