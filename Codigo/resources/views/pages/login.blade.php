3b7342bbd571512a8ca95882d049a6587ebb56af
José Maurício Guimarães França <josemauriciogf@gmail.com>
2021-04-05 21:12:21 -0300

    update login page - mobile and desktop version

--- Codigo/resources/views/pages/login.blade.php
+++ Codigo/resources/views/pages/login.blade.php
@@ -5,36 +5,54 @@
 @endsection
 
 @section('content')
-<div id="main" class="container-fluid">
+<div class="container-fluid p-0">
 
-    <div id="row" class="row">
+    <div id="main_img_mobile" class="d-md-none col-12 p-0">
+            <img class="park_mobile" src="/assets/img/park.jpeg" alt="park">
+            <img class="parkio_mobile" src="/assets/img/parkiowhite.png" alt="parkio">
+    </div>
 
-            <div id="form" class="col-12 col-md-0" form="cadastro">
+        <div id="line" class="row">
 
-                    <h1 class="login">Log in</h1>
-                    
-                <div id="user" class="inputWithIcon">
-                    <input type="text" placeholder="Usuário">
-                    <i class="fas fa-user-circle"></i>
+            <div class="col-12 col-md-4 p-0" form="cadastro">
+                
+                <div id="form">
+                    <h1 class="login">Log in</h1>  
+                    <div class="row">
+                        <div class="col-12">
+                            <div id="user" class="inputWithIcon">
+                                <input type="text" placeholder="Usuário">
+                                <i class="fas fa-user-circle"></i>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row mt-3">
+                        <div class="col-12">
+                            <div id="pass" class="inputWithIcon">
+                                <input type="text" placeholder="Senha">
+                                <i class="fas fa-lock"></i>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row mt-3">
+                        <div class="col-12">
+                            <button class="btn btn-secondary" type="submit" form="cadastro" >Entrar</button>
+                        </div>
+                    </div>
+
                 </div>
-
-                <div id="pass" class="inputWithIcon">
-                    <input type="text" placeholder="Senha">
-                    <i class="fas fa-lock"></i>
-                </div>
-
-                <button class="btn1" type="submit" form="cadastro" >Entrar</button>
 
             </div>
 
-
-            <div class="main_img" class="d-none d-md-inline col-0 col-md-8">
-                <img class="park" src="/assets/img/park.jpeg" alt="park" height=100%>
+            <div id="main_img" class="d-none d-md-inline col-0 col-md-8 p-0">
+                <img class="park" src="/assets/img/park.jpeg" alt="park">
                 <img class="parkio" src="/assets/img/parkiowhite.png" alt="parkio">
             </div>
 
+        </div>
 
-
-    </div>
 </div>
+
 @endsection
