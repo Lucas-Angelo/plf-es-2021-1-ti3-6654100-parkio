2d040538b5a3c85694a3f028dc5587d2b61cc49d
lcs2001 <lcs2001_@hotmail.com>
2021-04-08 16:31:10 -0300

    Update vehicle frontend

--- Codigo/resources/views/pages/gate.blade.php
+++ Codigo/resources/views/pages/gate.blade.php
@@ -69,7 +69,17 @@
                     
                 </form>
             </div>
-            <div class="tab-pane fade" id="exit" role="tabpanel" aria-labelledby="exit-tab">Saída</div>
+            <div class="tab-pane fade" id="exit" role="tabpanel" aria-labelledby="exit-tab">
+                <form onSubmit="handleExitFormSubmit(event)" class="row" id="exit-form">
+                    <div class="mb-3 col-12 col-md-4 col-lg-2">
+                        <label for="input-plate" class="form-label">Placa <span class="required">*</span></label>
+                        <input type="text" class="form-control" id="input-plate" required>
+                    </div>
+                    <div class="button-div text-center mt-5">
+                        <button class="btn" type="submit">Atualizar</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
     
