7204f588e97e18b325c2859be46025faa8c60b74
Henrique Monteiro <henrique.mont011@gmail.com>
2021-04-09 14:47:43 -0300

    fix problems

--- Codigo/app/Services/DestinationService.php
+++ Codigo/app/Services/DestinationService.php
@@ -9,29 +9,29 @@ use Illuminate\Support\Facades\DB;
 class DestinationService
 {
     
-    public function getAll($search = null){
-      $d = new Destination();
+  /** 
+   * Search for all gates (with pagination)
+   *
+   * @param String $search Search filter string to find in blocks and/or apartments
+   * @return Collection
+   */
+  public function getAll($search = null){
+    $d = new Destination();
 
-      if(!empty($search)) {
-        $d = $d->where(DB::raw("CONCAT(block,' ',apartament)"), 'LIKE' , '%'.$search.'%');
-      }
-
-      return $d->paginate(200);
+    if(!empty($search)) {
+      $d = $d->where(DB::raw("CONCAT(block,' ',apartament)"), 'LIKE' , '%'.$search.'%');
     }
 
+    return $d->paginate(200);
+  }
 
-    public function createDestination($block,int $apartament){
-      $destination = new Destination();
-      $destination->block = strtoupper($block);
-      $destination->apartament = $apartament;
-      $destination->save();
+  public function createDestination($block,int $apartament){
+    $destination = new Destination();
+    $destination->block = strtoupper($block);
+    $destination->apartament = $apartament;
+    $destination->save();
 
-      return $destination->id;
-    }
-
-
-
-
-
+    return $destination->id;
+  }
 
 }
