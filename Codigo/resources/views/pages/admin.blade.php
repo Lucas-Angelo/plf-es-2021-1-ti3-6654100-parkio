43ce0ed57c1adb1581ba9e4282e8b9e2257c96e6
Guilherme Gabriel <guilherme.gabriel.532@gmail.com>
2021-04-12 22:35:39 -0300

    add visitors category table

--- Codigo/resources/views/pages/admin.blade.php
+++ Codigo/resources/views/pages/admin.blade.php
@@ -3,6 +3,7 @@
 @section('extraassets')
     <link rel="stylesheet" href="{{ url('/assets/css/visitorCategory.css') }}" type="text/css">
     <link rel="stylesheet" href="{{ url('/assets/css/admin.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ url('/assets/css/userlist.css') }}" type="text/css">
 @endsection
 
 @section('content')
@@ -12,7 +13,6 @@
 </div>
     <div class="container-fluid">
 
-        
         <button class="mobile-menu-button d-flex d-md-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapseMenu" aria-expanded="false" aria-controls="collapseMenu">
             <i class="fas fa-align-justify"></i>Menu
         </button>
@@ -50,30 +50,43 @@
 
             <main class="col-12 col-md-9 tab-content">
                 <div class="tab-pane fade show active" id="gates" role="tabpanel" aria-labelledby="gates-tab">Portarias</div>
+
                 <div id="category" class="tab-pane fade" role="tabpanel" aria-labelledby="category-tab">
 
-                    <h3 class="text-center mb-3">
-                        Cadastro de Categoria de Visitante
-                    </h3>
-
-                    <form onSubmit="handleVisitorCategoryFormSubmit(event)" class="row" id="visitorCategory-form">
-
-                        <div class="mb-3 col-12 col-md-6">
-                            <label for="input-description" class="form-label">Descrição<span class="required">*</span></label>
-                            <input type="text" class="form-control" id="input-description" required>
+                    <div class="row">
+                        <div class="col-12 p-0 m-0">
+                            <div id="novo">
+                                <button class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#CreateCategoryModal"><i class="fas fa-plus"></i>Novo Usuário</button>
+                            </div>
                         </div>
-                        <div class="mb-3 col-12 col-md-6">
-                            <label for="input-time" class="form-label">Tempo padrão (minutos)<span class="required">*</span></label>
-                            <input type="number" min="0" class="form-control" id="input-time" required>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 p-0 m-0">
+                            <div id="tabela-usuario" class="d-none d-md-block">
+                                <table class="table sortable">
+                                    <colgroup>
+                                        <col span="1" style="width: 40%;">
+                                        <col span="1" style="width: 40%;">
+                                        <col span="1" style="width: 20%;">
+                                    </colgroup>
+                                    <thead>
+                                        <tr>
+                                            <th>Tipo</th>
+                                            <th>Tempo Recomendado</th>
+                                            <th>Ações</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="category-table-body">
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div id="lista-category" class="d-md-none mt-3">
+                            </div>
                         </div>
-
-                        <div class="button-div text-center mt-3">
-                            <button class="btn" type="submit">Cadastrar</button>
-                        </div>
-                    
-                    </form>
+                    </div>
                             
                 </div>
+
                 <div class="tab-pane fade" id="reported" role="tabpanel" aria-labelledby="reported-tab">Veículos Reportados</div>
 
 
@@ -82,7 +95,35 @@
         </div>
         
 
-        
+        <!--Modais-->
+        <div class="modal fade" id="CreateCategoryModal" tabindex="-1" aria-labelledby="CreateCategoryModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="CreateCategoryModalLabel">Cadastro de Categoria de Visitantes</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form onSubmit="handleVisitorCategoryFormSubmit(event)" class="row" id="visitorCategory-form">
+
+                            <div class="mb-3 col-12 col-lg-6">
+                                <label for="input-description" class="form-label">Descrição<span class="required">*</span></label>
+                                <input type="text" class="form-control" id="input-description" required autocomplete="false">
+                            </div>
+                            <div class="mb-3 col-12 col-lg-6">
+                                <label for="input-time" class="form-label">Tempo padrão (minutos)<span class="required">*</span></label>
+                                <input type="number" min="0" class="form-control" id="input-time" required>
+                            </div>
+
+                            <div class="button-div text-center mt-3">
+                                <button class="btn" type="submit">Cadastrar</button>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
         
     </div>
 @endsection
