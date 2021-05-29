5c1cc47d00994982b9bf5a4fe4a0414a0bd8f5f1
Victor Campos <victorboaventcampos@gmail.com>
2021-05-29 09:47:27 -0300

    web php updated

--- Codigo/routes/web.php
+++ Codigo/routes/web.php
@@ -57,6 +57,10 @@ $router->group(['prefix' => '/api'], function () use ($router) {
         //delays
         $router->get('/delay', ['uses' => 'DelayController@getAll', 'auth' => ['A', 'P']]); // Search for all vehicles delays
         $router->post('/delay', ['uses' => 'DelayController@create', 'auth' => ['A', 'P']]); // Create delay for a specific vehicle
+
+        $router->get('/reportVehicle', ['uses' => 'ReportController@getQtdVisitorByDate', 'auth' => ['A']]); //search qtde vehicles where into determinate date
+        $router->get('/reportGateKeeper', ['uses' => 'ReportController@getQtdVehiclesByGateKeeper', 'auth' => ['A']]); // search qtde vehicle and who let it in by date
+
     });
 });
 
