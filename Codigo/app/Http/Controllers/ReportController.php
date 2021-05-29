70b74bdf30b5aa94eaef82774ccb4bb2ac53f477
Victor Campos <victorboaventcampos@gmail.com>
2021-05-29 11:14:10 -0300

    report filters added

--- Codigo/app/Http/Controllers/ReportController.php
+++ Codigo/app/Http/Controllers/ReportController.php
@@ -8,9 +8,14 @@ use App\Services\ReportService;
 class ReportController extends Controller
 {
     public function getQtdVisitorByDate(Request $request){
+
+        $this->validate($request, [
+            'dates' => 'required|max:12',
+        ]);
+
         try {
             $r = new ReportService();
-            return $r->getQtdVisitorByDate($request->dates); //pass initial date
+            return $r->getQtdVisitorByDate($request->dates, $request->gate, $request->doorMen); //pass initial date
 
         } catch (\Exception $e) {
             return response()->json([
@@ -21,9 +26,14 @@ class ReportController extends Controller
 
 
     public function getQtdVehiclesByGateKeeper(Request $request){
+
+        $this->validate($request, [
+            'dates' => 'required|max:12',
+        ]);
+
         try {
             $r = new ReportService();
-            return $r->getQtdVehiclesByGateKeeper($request->dates); //pass initial date
+            return $r->getQtdVehiclesByGateKeeper($request->dates, $request->gate, $request->doorMen); //pass initial date
         } catch (\Exception $e) {
             return response()->json([
                 'error' => $e->getMessage()
--- Codigo/app/Services/ReportService.php
+++ Codigo/app/Services/ReportService.php
@@ -19,31 +19,37 @@ class ReportService
         return Gate::all();
     }
     
-    public function getQtdVisitorByDate($date)
+    public function getQtdVisitorByDate($date, $gate = null, $doorMen = null)
     {
         $list = new Vehicle();
         $arrayday = array();
         $arrayhour = array();
         //dd(strtotime("+7 day", strtotime($date)));
         //dd(date("Y-m-d", strtotime("+7 day", strtotime($date) )));
-
+        $vehicles = new Vehicle();
         $begin = new DateTime( $date );
         $end   = new DateTime( date("Y-m-d", strtotime("+7 day", strtotime($date))) );
         
         for($i = $begin; $i < $end; $i->modify('+1 day')){
-
-                $x = 0;
-                $vehicles = new Vehicle();
+               
                 $arrayhour = [];
-                for($x = 0; $x < 8 ; $x++){
+                for($x = 0; $x < 24 ; $x+=3){
+                    
                     $vehicles = Vehicle::
                               whereRaw('date(created_at) = ?', $i->format("Y-m-d"))
                             ->whereRaw('hour(created_at) >= ?', $x)
-                            ->whereRaw('hour(created_at) <= ?', $x+3)
-                            ->count('id');
+                            ->whereRaw('hour(created_at) <= ?', $x+3);
+                            
+                    if(!empty($gate)){
+                        $vehicles = $vehicles->where('gate_id', $gate);          
+                    }    
+                    
+                    if(!empty($doorMen)){
+                        $vehicles = $vehicles->where('user_in_id', $doorMen);          
+                    }    
 
-                    array_push($arrayhour, $vehicles);
-            }
+                    array_push($arrayhour, $vehicles->count('id'));
+                }
 
             array_push($arrayday,  $arrayhour);
 
@@ -54,7 +60,7 @@ class ReportService
     }
 
 
-    public function getQtdVehiclesByGateKeeper($date)
+    public function getQtdVehiclesByGateKeeper($date, $gate = null, $doorMen = null)
     {
         $list = new Vehicle();
         $arrayday = array();
@@ -69,11 +75,15 @@ class ReportService
                               Select(DB::raw("count(vehicle.id), user_in_id, name"))
                               ->join('user', 'user.id', '=', 'user_in_id')
                             ->whereRaw('date(vehicle.created_at) = ?', $i->format("Y-m-d"))
-                            ->groupBy('user_in_id', 'name')
-                            ->get()
-                            ->toArray();
+                            ->groupBy('user_in_id', 'name');
+                    if(!empty($gate)){
+                        $gateKeeper = $gateKeeper->where('gate_id', $gate);
+                    }
+                    if(!empty($doorMen)){
+                        $gateKeeper = $gateKeeper->where('user_in_id', $doorMen);
+                    }
             
-            array_push($arrayday,  $gateKeeper);
+            array_push($arrayday,  $gateKeeper->get()->toArray());
 
          }
 
