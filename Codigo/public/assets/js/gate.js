0be1b935f1d0c20f3c9e96ea974c9b2675f3ba24
Guilherme Gabriel <guilherme.gabriel.532@gmail.com>
2021-06-05 11:32:34 -0300

    validacao de cpf adicionada

--- Codigo/public/assets/js/gate.js
+++ Codigo/public/assets/js/gate.js
@@ -3,6 +3,27 @@ var defaultTime = 30;
 let currentPlate = ''
 let currentId = null
 
+function TestaCPF(strCPF) {
+    var Soma;
+    var Resto;
+    Soma = 0;
+  if (strCPF == "00000000000") return false;
+
+  for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+  Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
+
+  Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
+    return true;
+}
+
 const setTime = (time) => {
     document.querySelector('#input-time').value = time;
 }
@@ -337,6 +358,19 @@ const handleEntranceFormSubmit = (event) => {
     const cpf = document.querySelector("#input-cpf").value;
     const color = document.querySelector("#input-color").value;
     const gateId = location.pathname.split('/')[2]; //Gate ID
+
+    const cpfOnlyNumbers = cpf.replaceAll('.', '').replaceAll('-', '') 
+    console.log(cpfOnlyNumbers)
+    if(cpf && !TestaCPF(cpfOnlyNumbers)){
+        Swal.fire({
+            title: 'Aviso',
+            html: `O CPF ${cpf} é inválido<br><br>
+                    Corrija-o ou deixe o campo vazio`,
+            icon: 'warning',
+            confirmButtonText: 'OK'
+        })
+        return
+    }
 
 
     const data = {
